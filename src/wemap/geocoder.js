f83a53451f0ef32a121642f86baddc8f944eed2a
Vu Van Quy <vuvanquy1998realkio@gmail.com>
2020-02-11 17:52:03 +0700

    init geocoder

--- src/wemap/geocoder.js
+++ src/wemap/geocoder.js
@@ -1,18 +1,33 @@
+import PeliasGeocoder from '../pelias-geocoder/pelias-geocoder'
 export default class WeGeocoder {
 
-    constructor(options) {
-        options = options || {};
+        constructor(options) {
+                options = options || {};
+                this.options = options;
+                this.map = this.options.map
+                this.init();
+        }
 
-        this.options = options;
-
-        this.init();
-    }
-
-    /**
-     * Returns the origin of the current route.
-     * @returns {Object} origin
-     */
-    init(){
-        console.log('init')
-    }
+        /**
+         * Returns the origin of the current route.
+         * @returns {Object} origin
+         */
+        init(){
+            console.log('init geocoder')
+            let engine = this.options.engine
+            if(engine=='default'||!engine||engine=='pelias'){
+                let geocoder = new PeliasGeocoder({
+                    params: {'access-token': this.options.accessToken},
+                    flyTo: 'hybrid',
+                    wof: true,
+                    url: 'https://places.jawg.io/v1',
+                    useFocusPoint: true,
+                    marker: {
+                        icon: iconMarkerEl,
+                        multiple: false
+                    },
+                    });
+                this.map.addControl(geocoder)
+            }
+        }
 }
