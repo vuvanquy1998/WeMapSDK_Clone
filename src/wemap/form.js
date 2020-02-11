0a9d26277fb2762f7eb2645b14e1c4218d51b9a2
Hoang Xuan Phuong <xuanphuong3105@gmail.com>
2020-02-11 11:51:31 +0700

    Added read json file

--- src/wemap/form.js
+++ src/wemap/form.js
@@ -1,9 +1,13 @@
+// Get data form json file
+const json = require('../config.json');
+
 /**
  * Returns getWeMapForm.
  * @returns {String} '@WeMap Form'
  */
 function getWeMapForm() {
-    return '<form class="wemap-form"> Hello </form>';
+    console.log(json);
+    return '<form class="wemap-form"> Hello:  </form>';
 }
 
 if (typeof module !== 'undefined') { module.exports = getWeMapForm; }
