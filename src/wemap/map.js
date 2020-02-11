251d51b970c731ccb67aee628947296d97e58651
HieuTM <hieutm@fimo.edu.vn>
2020-02-11 14:36:15 +0700

    wemapgl.WeMap init with default values

--- src/wemap/map.js
+++ src/wemap/map.js
@@ -1,15 +1,52 @@
 import getWeMapForm from './form';
 
 export default class WeMap {
-
     constructor(options) {
         options = options || {};
-
-        this.options = {};
-
-        if (options.params) {
-            this.params = options.params;
+        this.defaultOptions = {
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [105.8550736, 21.0283243],
+            zoom: 13,
+            reverse: false
         }
+        this.options = options;
+        this.setDefaultValue(this.defaultOptions, this.options);
+        this.init();
+    }
+    
+    /**
+     * Init
+     */
+    init() {
+
+        this.map = new wemapgl.Map({
+                container: this.options.container,
+                style: this.options.style,
+                center: this.options.center,
+                zoom: this.options.zoom
+        });
+        if(this.options.reverse) {
+            // TODO:
+        }
+        console.log(this.options);
+        
+    }
+
+    setDefaultValue(defaultOptions, options) {
+        for (var p in defaultOptions) {
+            if(options.hasOwnProperty(p)) {
+                if(options === "default") {
+                    options[p] = defaultOptions[p];
+                }
+            } else {
+                options[p] = defaultOptions[p];
+            }
+        }
+    }
+
+    lookup(info) {
+        info = info || {};
+        // TODO: Call lookup API
     }
 
     /**
