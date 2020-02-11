6bb5d8ea7c03b353d3466138b4b0f8ca2cf4a5ae
HieuTM <hieutm@fimo.edu.vn>
2020-02-11 21:48:08 +0700

    Change cursor when mouse-over POIs

--- src/wemap/map.js
+++ src/wemap/map.js
@@ -1,17 +1,25 @@
+import {getJSON} from '../util/ajax';
 import getWeMapForm from './form';
 
 export default class WeMap {
     constructor(options) {
         options = options || {};
+
+        this.styleLinks = {
+            "bright" : "https://apis.wemap.asia/vector-tiles/styles/osm-bright/style.json?key=IqzJukzUWpWrcDHJeDpUPLSGndDx",
+            "dark" : "https://apis.wemap.asia/vector-tiles/styles/osm-bright/style.json?key=IqzJukzUWpWrcDHJeDpUPLSGndDx",
+        }
+
         this.defaultOptions = {
-            style: "https://apis.wemap.asia/vector-tiles/styles/osm-bright/style.json?key=IqzJukzUWpWrcDHJeDpUPLSGndDx",
+            // TODO: get link from config file
+            style: "bright",
             center: [105.8550736, 21.0283243],
             zoom: 13,
             reverse: false,
             attributionControl: false
         }
+
         this.options = options;
-        this.setDefaultValue(this.defaultOptions, this.options);
         this.init();
     }
     
@@ -20,9 +28,19 @@ export default class WeMap {
      */
     init() {
 
+        for (var p in this.defaultOptions) {
+            if(this.options.hasOwnProperty(p)) {
+                if(this.options === "default") {
+                    this.options[p] = this.defaultOptions[p];
+                }
+            } else {
+                this.options[p] = this.defaultOptions[p];
+            }
+        }
+        
         this.map = new wemapgl.Map({
                 container: this.options.container,
-                style: this.options.style,
+                style: this.styleLinks[this.options.style],
                 center: this.options.center,
                 zoom: this.options.zoom,
                 attributionControl: this.options.attributionControl
@@ -33,27 +51,42 @@ export default class WeMap {
             customAttribution: ["© WeMap"]
         }));
 
+        getJSON({
+            url: this.styleLinks[this.options.style],
+            method: 'GET'
+        }, (error, data) => {
+            var point_layers = [];
+            for(let i = 0; i < data.layers.length; i++){
+                let layer_id = data.layers[i].id
+                if(layer_id.includes('poi')){
+                    point_layers.push(layer_id);
+                }
+            }
+            point_layers.forEach((label, index) => {
+                this.map.on('mouseover', label, (e) => {
+                    this.map.getCanvas().style.cursor = 'pointer';
+                })
+                this.map.on('mouseleave', label, (e) => {
+                    this.map.getCanvas().style.cursor = '';
+                })
+            })
+        });
+
         if(this.options.reverse) {
             // TODO:
         }
         
     }
 
-    setDefaultValue(defaultOptions, options) {
-        for (var p in defaultOptions) {
-            if(options.hasOwnProperty(p)) {
-                if(options === "default") {
-                    options[p] = defaultOptions[p];
-                }
-            } else {
-                options[p] = defaultOptions[p];
-            }
-        }
-    }
-
     lookup(info) {
         info = info || {};
         // TODO: Call lookup API
+        return {};
+    }
+
+    doSomething(something) {
+        console.log("Hello!");
+        console.log(something);
     }
 
     /**
