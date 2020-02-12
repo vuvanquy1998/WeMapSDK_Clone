ceedf9cb8e43505d42d5fa4a4a83c1fc2d7d0111
HieuTM <hieutm@fimo.edu.vn>
2020-02-12 16:39:53 +0700

    wemapgl.API

--- src/wemap/api.js
+++ src/wemap/api.js
@@ -5,19 +5,21 @@ export default class API {
      * Returns detail infomation of an OSM Id with OSM type
      * @returns {Object} detail infomation
      */
-    static lookup({osmId, osmType, key}) {
-        var result = 1000;
-        getJSON({
-            // TODO: get lookup api
-            url: 'https://apis.wemap.asia/we-tools/lookup?key=' + key + "&id=" + osmType + osmId,
+    static lookup({osmId, osmType, key}, cb) {
+        // TODO: get lookup api
+        if(osmType != null && osmType != undefined && osmType != "") {
+            var url = "https://apis.wemap.asia/we-tools/lookup?key=" + key + "&id=" + osmType + osmId;
+        } else {
+            var url = "https://apis.wemap.asia/we-tools/lookup/" + osmId + "?key=" + key;
+        }
+        
+        console.log(url);
+        getJSON({ 
+            url: url,
             method: 'GET'
         }, (error, data) => {
-            // TODO: handle error'
-            console.log("Hello " + result);
-            result = data;
-            return result;
+            // TODO: handle error
+            cb(data);
         });
-
-        return result;
     }
 }
