0055d80c9734cf4545aa57ea23de0b6a76fa8aa4
ThaoNguyen99-fimo <thaonp@fimo.edu.vn>
2020-02-21 10:07:33 +0700

    reconstruct code

--- src/wemap/rightclick.js
+++ src/wemap/rightclick.js
@@ -1,10 +1,10 @@
 import { getJSON } from '../util/ajax'
 import { default as config } from '../config.json';
+import PlaceDetail from './placeDetail'
 
 export default class RightClick {
-    constructor(map, stt, key) {
+    constructor(map, key) {
         this.map = map;
-        this.stt = stt;
         this.key = key;
         this.initView();
         this.init();
@@ -19,7 +19,7 @@ export default class RightClick {
             '</div>'
         document.body.appendChild(rightClick)
         let showBottomDetail = document.createElement('div')
-        showBottomDetail.innerHTML = '<div id="place">' +
+        showBottomDetail.innerHTML = '<div id="place" style="display: none">' +
             '<div class="class-pl">' +
             '<div class="image-place">' +
             '<img class="placeicon" id="preview-image" src="https://map.fimo.com.vn/assets/images/no_street.png">' +
@@ -43,139 +43,143 @@ export default class RightClick {
         document.body.appendChild(showBottomDetail)
     }
     init() {
-        if (this.stt) {
-            let chosen_point_info = {}
-            this.map.on('contextmenu', (e) => {
-                let mouseX = e.point.x;
-                let mouseY = e.point.y;
-                // let max_width = $(window).width();
-                let max_width = window.innerWidth;
-                // let max_height = $(window).height();
-                let max_height = window.innerHeight;
-                // let context_menu_width = $('#right-click-menu-container').width();
-                let context_menu_width = document.getElementById("right-click-menu-container").clientWidth;
-                //let context_menu_height = $('#right-click-menu-container').height();
-                let context_menu_height = document.getElementById("right-click-menu-container").clientHeight;
-                let translateX = 0;
-                let translateY = 0;
+        let received_data = {}
+        let point_layers = []
 
-                if (mouseX + context_menu_width > max_width) {
-                    console.log('qua dai ngang')
-                    translateX = max_width - context_menu_width;
-                } else {
-                    translateX = mouseX;
+        this.map.on('load', (e) => {
+            getJSON({
+                url: `${config.style.bright}${this.key}`,
+                method: 'GET'
+            }, (err, data) => {
+                for(let i = 0; i < data.layers.length; i++){
+                    let layer_id = data.layers[i].id
+                    if(layer_id.includes('poi')){
+                        point_layers.push(layer_id)
+                    }
                 }
-                if (mouseY + context_menu_height > max_height) {
-                    console.log('qua dai doc')
-                    translateY = max_height - context_menu_height;
-                } else {
-                    translateY = mouseY;
+            })
+        })
+        this.map.on('click', (e) => {
+            display_ui('right-click-menu-container', 'none')
+        })
+        this.map.on('contextmenu', (e) => {
+            let mouseX = e.point.x;
+            let mouseY = e.point.y;
+            let max_width = window.innerWidth;
+            let max_height = window.innerHeight;
+            let context_menu_width = document.getElementById("right-click-menu-container").clientWidth;
+            let context_menu_height = document.getElementById("right-click-menu-container").clientHeight;
+            let translateX = 0;
+            let translateY = 0;
+
+            if (mouseX + context_menu_width > max_width) {
+                translateX = max_width - context_menu_width;
+            } else {
+                translateX = mouseX;
+            }
+            if (mouseY + context_menu_height > max_height) {
+                translateY = max_height - context_menu_height;
+            } else {
+                translateY = mouseY;
+            }
+            display_ui('right-click-menu-container', 'block')
+            document.getElementById("right-click-menu-container").style.transform = `translate(${translateX}px,${translateY}px)`
+            this.clicked_poi = e;
+        })
+        document.getElementById('right-click-reverse').addEventListener('click', (event) => {
+            let features = this.map.queryRenderedFeatures(this.clicked_poi.point)
+
+            getJSON({
+                url: `${config.reverse}${this.key}&lat=${this.clicked_poi.lngLat.lat}&lon=${this.clicked_poi.lngLat.lng}`,
+                method: 'GET'
+            }, (err, data) => {
+                received_data = data.features[0]
+            
+                function not_point_render_detail(info, geometry) {
+                    display_ui('place', 'block')
+                    let address = [info.name, info.street, info.district, info.city, info.country]
+                    let second_line = []
+                    let last_i = 0
+                    for (let i = 0; i < 5; i++) {
+                        let unit = address[i]
+                        if (unit) {
+                            document.getElementById('placename').innerHTML = unit
+                            last_i = i
+                            break
+                        }
+                    }
+                    for (let j = last_i + 1; j < 5; j++) {
+                        let unit = address[j]
+                        if (unit) {
+                            second_line.push(unit)
+                        }
+                    }
+                    document.getElementById('placeadd').innerHTML = second_line.join(', ')
+                    document.getElementById('placelatlon').innerHTML = Number(geometry.coordinates[1]).toFixed(7) + ' ,' + Number(geometry.coordinates[0]).toFixed(7)
                 }
-                // $('#right-click-menu-container').css({ 'display': "block" })
-                document.getElementById("right-click-menu-container").style.display = "block"
-                //$('#right-click-menu-container').css({ "transform": `translate(${translateX}px,${translateY}px)` })
-                document.getElementById("right-click-menu-container").style.transform = `translate(${translateX}px,${translateY}px)`
-                this.clicked_poi = e;
-            })
 
-            this.map.on('click', (e) => {
-                // $('#right-click-menu-container').css({ 'display': "none" })
-                document.getElementById("right-click-menu-container").style.display = "none";
-            })
-
-            document.getElementById('right-click-reverse').addEventListener('click', (event) => {
-                let features = this.map.queryRenderedFeatures(this.clicked_poi.point);
-                console.log('feature', features)
-
-                //addMarkder(e.lngLat, e.lngLat.lat)
-
-                let point_layers = ['poi-level-1', 'poi-level-2', 'poi-level-3', 'poi-level-4']
-
-                getJSON({
-                    url: `${config.reverse}${this.key}&lat=${this.clicked_poi.lngLat.lat}&lon=${this.clicked_poi.lngLat.lng}`,
-                    method: 'GET'
-                }, (err, data) => {
-                    console.log('ajax data', data)
-
-                    //kết qủa reverse được lấy để hiển thị
-                    function choose_received_data(point, mapbox_point_name_en, mapbox_point_name_vi) {
-                        return data.features[0]
-                    }
-
-                    //ui cho reverse không phải icon trên map
-                    function not_point_render_detail(info, geometry) {
-                        console.log('kp điểm:', info.name, info.street, info.district, info.city, info.country)
-
-                        // document.getElementById('detail-feature').style.display = "none"
-
-                        document.getElementById('place').style.display = 'block';
-                        let address = [info.name, info.street, info.district, info.city, info.country]
-                        let second_line = []
-                        let last_i = 0
-                        for (let i = 0; i < 5; i++) {
-                            let unit = address[i]
-                            if (unit) {
-                                document.getElementById('placename').innerHTML = unit
-                                last_i = i
-                                break
-                            }
+    
+                if (features.length === 0) {
+                    not_point_render_detail(received_data.properties, received_data.geometry)
+                    display_ui('right-click-menu-container', 'none')
+                }
+                else {
+                    let not_point_layer = 0
+                    features.forEach((feature, index) => {
+                        if (point_layers.includes(feature.layer.id)) {
+                            showDetailFeature();
+                            display_ui('place', 'none')
+                            not_point_layer += 1
                         }
-                        for (let j = last_i + 1; j < 5; j++) {
-                            let unit = address[j]
-                            if (unit) {
-                                second_line.push(unit)
-                            }
-                        }
-                        document.getElementById('placeadd').innerHTML = second_line.join(', ')
-                        document.getElementById('placelatlon').innerHTML = Number(geometry.coordinates[1]).toFixed(7) + ' ,' + Number(geometry.coordinates[0]).toFixed(7)
+                    });
+                    if (not_point_layer == 0) {
+                        not_point_render_detail(received_data.properties, received_data.geometry)
                     }
-
-                    //hiển thị ui reverse
-
-                    if (features.length === 0) {
-                        console.log('van dung ne')
-                        let chosen_info = choose_received_data(false, null, null)
-                        not_point_render_detail(chosen_info.properties, chosen_info.geometry)
-                        chosen_point_info = chosen_info
-                        // $('#right-click-menu-container').css({ 'display': "none" })
-                        document.getElementById('right-click-menu-container').style.display = "none"
-                    }
-                    else {
-                        let not_point_layer = 0
-                        features.forEach((feature, index) => {
-                            if (point_layers.includes(feature.layer.id)) {
-                                let place_name_vi = features[index].properties.name
-                                let place_name_en = features[index].properties.name_en
-
-
-                                let chosen_info = choose_received_data(true, place_name_en, place_name_vi)
-
-                                not_point_layer += 1
-                                console.log('chính là điểm:', chosen_info.properties.name)
-                                console.log('gọi hàm của Quý')
-                            }
-                        });
-                        if (not_point_layer == 0) {
-                            let chosen_info = choose_received_data(false, null, null)
-                            not_point_render_detail(chosen_info.properties, chosen_info.geometry)
-                            chosen_point_info = chosen_info
-                        }
-                        // $('#right-click-menu-container').css({ 'display': "none" })
-                        document.getElementById("right-click-menu-container").style.display = "none"
-                    }
-                })
+                    display_ui('right-click-menu-container', 'none')
+                }
             })
-            document.getElementById('click-detail').addEventListener('click', (e) => {
-                // let place = new PlaceDetail({name: chosen_point_info.properties.name, type: chosen_point_info.type, lat: chosen_point_info.geometry.coordinates[1], lon: chosen_point_info.geometry.coordinates[0],address: [chosen_point_info.properties.housenumber,chosen_point_info.properties.street, chosen_point_info.properties.district, chosen_point_info.properties.city, chosen_point_info.properties.country],osm_id: chosen_point_info.properties.osm_id, osm_type: chosen_point_info.properties.osm_type});
-                // place.showDetailFeature()
-                document.getElementById('place').style.display = 'none'
-            })
+        })
+        document.getElementById('click-detail').addEventListener('click', (e) => {
+            showDetailFeature()
+            display_ui('place', 'none')
+        })
 
-            document.getElementById('placeclose').addEventListener('click', (e) => {
-                // deleteUrlParam('rx');
-                // deleteUrlParam('ry');
-                document.getElementById('place').style.display = "none"
-            })
+        document.getElementById('placeclose').addEventListener('click', (e) => {
+            display_ui('place', 'none')
+        })
+        function showDetailFeature(){
+            // let place = new PlaceDetail({
+            //     name: received_data.properties.name, 
+            //     type: received_data.type, 
+            //     lat: received_data.geometry.coordinates[1], 
+            //     lon: received_data.geometry.coordinates[0],
+            //     address: [
+            //         received_data.properties.housenumber,
+            //         received_data.properties.street, 
+            //         received_data.properties.district, 
+            //         received_data.properties.city, 
+            //         received_data.properties.country
+            //     ],
+            //     osm_id: received_data.properties.osm_id, 
+            //     osm_type: received_data.properties.osm_type
+            // });
+            // place.showDetailFeature()
         }
+        function display_ui(id, action){
+            document.getElementById(id).style.display = action;
+        }
+    }
+    static add(id, text){
+        let new_right_click_item = document.createElement('div')
+        new_right_click_item.setAttribute('id', id)
+        new_right_click_item.setAttribute('class', 'right-click-menu-item')
+        new_right_click_item.innerText = text
+
+        document.getElementById('right-click-menu-container').appendChild(new_right_click_item)
+    }
+    static rightClickCoordinates(){
+        map.on('contextmenu', (e) => {
+            console.log(e.lngLat.lng, e.lngLat.lat)
+        })
     }
 }
