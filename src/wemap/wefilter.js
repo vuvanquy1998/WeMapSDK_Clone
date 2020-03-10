8b360e9254714bf2f702ee2065ab8bdf24da5ed3
HieuTM <hieutm@fimo.edu.vn>
2020-03-10 20:14:36 +0700

    hieutm

--- src/wemap/wefilter.js
+++ src/wemap/wefilter.js
@@ -1,51 +1,54 @@
 export default class WeFilterControl {
     constructor(options) {
         
-        this._groups = [
-            {
-                "Ẩm thực": {
-                    "color": "red",
-                    "classes": ["restaurant", "fast_food"]
-                }
+        this._groups = {
+            "Ẩm thực": {
+                "color": "red",
+                "classes": ["restaurant", "fast_food"]
             },
-            {
-                "Khách sạn": {
-                    "color": "black",
-                    "classes": ["lodging", "hotel"]
-                }
+
+            "Khách sạn": {
+                "color": "black",
+                "classes": ["lodging", "hotel"]
             },
-            {
-                "Giải trí": {
-                    "color": "blue",
-                    "classes": ["cafe", "bar"]
-                }
+
+            "Giải trí": {
+                "color": "blue",
+                "classes": ["cafe", "bar"]
             },
-            {
-                "Mua sắm": {
-                    "color": "blue",
-                    "classes": ["shop", "grocery", "clothing_store"]
-                }
+
+            "Mua sắm": {
+                "color": "blue",
+                "classes": ["shop", "grocery", "clothing_store"]
             }
-        ];
+        };
 
         this._poiLayers = ["poi-level-1-en", "poi-level-1", "poi-level-2-en", "poi-level-2", "poi-level-3-en", "poi-level-3"];
         this._choosedGroup = "";
+        this.markers = [];
     }
 
     renderView() {
-        // get all features that match choosed group
         if(this._choosedGroup != "") {
             const matchedFeatures = [];
-            this._map.queryRederedFeatures({
+            this._map.queryRenderedFeatures({
                 layers: this._poiLayers
-            }).forEach(feature => (this._groups[this._choosedGroup]["classes"].includes(feature.properties.class) && matchedFeatures.push(feature)));
-            console.log(matchedFeatures);
+            }).forEach(feature => {
+                (this._groups[this._choosedGroup]["classes"].includes(feature.properties.class) && matchedFeatures.push(feature));
+            });
+            console.log(matchedFeatures)
+            this.addMarkers(matchedFeatures);
+        } else {
+
         }
     }
 
     onAdd(map) {
         this._map = map;
         this._container = document.createElement('div');
+        Object.keys(this._groups).forEach(key => {
+            this._container.appendChild(this.createGroupButton(key));
+        });
         this._container.setAttribute("id", "wemap-ctrl-filter");
         this._map.on('idle', () => {
             this.renderView();
@@ -60,6 +63,42 @@ export default class WeFilterControl {
 
     getDefaultPosition() {
         return 'top-right';
+    }
+
+    addMarkers(features) {
+        features.forEach(feature => {
+            const markerEl = document.createElement("div");
+            markerEl.innerHTML =
+                "<div class='wemap-marker-arrow'></div>" +
+                "<div class='wemap-marker-pulse'></div>";
+
+            // TODO: add marker
+            console.log("add marker!");
+
+        });
+    }
+
+    removeMarkers(group) {
+        // TODO: remove marker
+    }
+
+    createGroupButton(group) {
+        const button = document.createElement("button");
+        button.setAttribute("class", "wemap-ctrl-filter-button");
+        button.innerHTML = group;
+        button.addEventListener('click', () => {
+            if(this._choosedGroup == group) {
+                this._choosedGroup = "";
+                this.removeMarkers(group);
+            } else {
+                if(this._choosedGroup != "") {
+                    this.removeMarkers(this._choosedGroup);
+                }
+                this._choosedGroup = group;
+                this.renderView();
+            }
+        });
+        return button;
     }
 
 }
