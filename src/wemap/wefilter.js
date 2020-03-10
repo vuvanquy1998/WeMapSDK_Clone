ad2175fdc4b5db064d02e9ce82443e6b5630c5c9
HieuTM <hieutm@fimo.edu.vn>
2020-03-10 09:59:18 +0700

    update

--- src/wemap/wefilter.js
+++ src/wemap/wefilter.js
@@ -1,38 +1,46 @@
 export default class WeFilterControl {
     constructor(options) {
+        
+        this._groups = [
+            {
+                "Ẩm thực": {
+                    "color": "red",
+                    "classes": ["restaurant", "fast_food"]
+                }
+            },
+            {
+                "Khách sạn": {
+                    "color": "black",
+                    "classes": ["lodging", "hotel"]
+                }
+            },
+            {
+                "Giải trí": {
+                    "color": "blue",
+                    "classes": ["cafe", "bar"]
+                }
+            },
+            {
+                "Mua sắm": {
+                    "color": "blue",
+                    "classes": ["shop", "grocery", "clothing_store"]
+                }
+            }
+        ];
 
+        this._poiLayers = ["poi-level-1-en", "poi-level-1", "poi-level-2-en", "poi-level-2", "poi-level-3-en", "poi-level-3"];
+        this._choosedGroup = "";
     }
 
     renderView() {
-        // TODO: check if viewport has only 1 feature, dont render "Show all classes" radio, checked that radio immediately
-
-        this._poiLayers = ["poi-level-1-en", "poi-level-1", "poi-level-2-en", "poi-level-2", "poi-level-3-en", "poi-level-3"];
-
-        this._container.innerHTML = "";
-
-        const features = this._map.queryRenderedFeatures({
-            layers: this._poiLayers
-        });
-        if(features.length > 0) {
-            const showAllClassesRadio = this.createRadioGroup("radio-feature-class-all", "radio-feature-class", "Show all classes");
-            showAllClassesRadio.addEventListener('click', () => this.showAllClasses());
-            this._container.appendChild(showAllClassesRadio);
-            
-            const featureClasses = new Set();
-            features.forEach(feature => {
-                featureClasses.add(feature.properties.class);
-            });
-
-            featureClasses.forEach(featureClass => {
-                const featureClassRadio = this.createRadioGroup("radio-feature-class-" + featureClass, "radio-feature-class", featureClass);
-                featureClassRadio.addEventListener("click", () => this.showSpecifiedClass(featureClass));
-                this._container.appendChild(featureClassRadio);
-            });
-        } else {
-            this._container.innerHTML = "No feature rendered";
+        // get all features that match choosed group
+        if(this._choosedGroup != "") {
+            const matchedFeatures = [];
+            this._map.queryRederedFeatures({
+                layers: this._poiLayers
+            }).forEach(feature => (this._groups[this._choosedGroup]["classes"].includes(feature.properties.class) && matchedFeatures.push(feature)));
+            console.log(matchedFeatures);
         }
-
-        
     }
 
     onAdd(map) {
@@ -54,47 +62,4 @@ export default class WeFilterControl {
         return 'top-right';
     }
 
-    createRadioGroup(id, name, value, checked) {
-        const radio = document.createElement("input");
-        radio.setAttribute("type", "radio");
-        radio.setAttribute("id", id);
-        radio.setAttribute("name", name);
-        radio.setAttribute("value", value);
-        checked && radio.setAttribute("checked", "checked");
-
-        const label = document.createElement('label');
-        label.setAttribute("for", id);
-        label.innerHTML = value;
-
-        const radioGroupContainer = document.createElement('div');
-        radioGroupContainer.appendChild(radio);
-        radioGroupContainer.appendChild(label);
-        radioGroupContainer.appendChild(document.createElement("br"));
-
-        return radioGroupContainer;
-    }
-
-    createCheckboxGroup() {
-
-    }
-
-    showSpecifiedClass(className) {
-        this._poiLayers.forEach(layer => {
-            this._map.setFilter(layer, ["==", "class", className]);
-        });
-    }
-
-    showMultipleClass(classNames) {
-    
-    }
-
-    showAllClasses() {
-        this._poiLayers.forEach(layer => {
-            this._map.setFilter(layer, null);
-        });
-    }
-
-    hideAllClasses() {
-        
-    }
 }
