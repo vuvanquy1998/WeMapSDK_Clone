b9d7c327149d068dc6a6cfd35b2d26782152fe55
HieuTM <hieutm@fimo.edu.vn>
2020-03-19 09:22:34 +0700

    update wefilter: use explore API

--- src/wemap/wefilter.js
+++ src/wemap/wefilter.js
@@ -1,5 +1,34 @@
+import { makeRequest } from '../util/ajax';
 export default class WeFilterControl {
     constructor(options) {
+        this._currentGroup = null;
+        this._userLocation = null;
+        this._groups = {
+            "cuisine": {
+                "text": "Ẩm thực",
+                "fa-icon": "fa-cutlery",
+                "k": "amenity",
+                "v": "restaurant"
+            },
+            "hotel": {
+                "text": "Nhà nghỉ",
+                "fa-icon": "fa-hotel",
+                "k": "tourism",
+                "v": "hotel"
+            },
+            "entertainment": {
+                "text": "Giải trí",
+                "fa-icon": "fa-glass",
+                "k": "amenity",
+                "v": "nightclub"
+            },
+            "shopping": {
+                "text": "Mua sắm",
+                "fa-icon": "fa-shopping-bag",
+                "k": "amenity",
+                "v": "nightclub"
+            }
+        };
     }
 
     onAdd(map) {
@@ -14,41 +43,24 @@ export default class WeFilterControl {
 
         wefilterContainer.appendChild(wefilterTitle);
 
-        let groups = {
-            "cuisine": {
-                "text": "Ẩm thực",
-                "fa-icon": "fa-cutlery"
-            },
-            "hotel": {
-                "text": "Nhà nghỉ",
-                "fa-icon": "fa-hotel"
-            },
-            "entertainment": {
-                "text": "Giải trí",
-                "fa-icon": "fa-glass"
-            },
-            "shopping": {
-                "text": "Mua sắm",
-                "fa-icon": "fa-shopping-bag"
-            }
-        };
-
-        Object.keys(groups).forEach(key => {
+        Object.keys(this._groups).forEach(group => {
             let wefilterButtonContainer = document.createElement("div");
             wefilterButtonContainer.setAttribute("class", "wefilter-button-container");
+            wefilterButtonContainer.setAttribute("id", "wefilter-button-container-" + group);
 
             let button = document.createElement("button");
             button.setAttribute("class", "wefilter-button");
-            button.setAttribute("id", "wefilter-button-" + key);
+            button.setAttribute("id", "wefilter-button-" + group);
 
             let icon = document.createElement("i");
-            icon.setAttribute("class", "fa " + groups[key]["fa-icon"]);
+            icon.setAttribute("class", "fa " + this._groups[group]["fa-icon"]);
             button.appendChild(icon);
             
             wefilterButtonContainer.appendChild(button);
+            wefilterButtonContainer.addEventListener("click", () => this.onGroupClick(group));
 
             let span = document.createElement("span");
-            span.innerHTML = groups[key]["text"];
+            span.innerHTML = this._groups[group]["text"];
             wefilterButtonContainer.appendChild(span);
 
             wefilterContainer.appendChild(wefilterButtonContainer);
@@ -66,5 +78,74 @@ export default class WeFilterControl {
     getDefaultPosition() {
         return 'top-right';
     }
+
+    onGroupClick(group) {
+        if(this._userLocation == null) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this._userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                this.explore(group);
+            }, () => {
+                this.printError("Không thể lấy vị trí của bạn");
+            });
+        } else {
+            this.explore(group);
+        }
+        
+    }
+
+    explore(group) {
+        if(this._currentGroup == null) { // user want to turn on
+            this._currentGroup = group;
+            document.getElementById("wefilter-button-container-" + group).classList.add("wefilter-button-container-active");
+            this.queryFeatures(group);
+        } else {
+            document.getElementById("wefilter-button-container-" + this._currentGroup).classList.remove("wefilter-button-container-active");
+            if(this._currentGroup != group) { // user want to change
+                this.queryFeatures(group);
+                this._currentGroup = group;
+                document.getElementById("wefilter-button-container-" + group).classList.add("wefilter-button-container-active");
+            } else { // user just click again to turn off
+                this._currentGroup = null;
+            }
+        }
+    }
+
+    printError(str) {
+        this.clearError();
+        let errorContainer = document.createElement("div");
+        errorContainer.setAttribute("id", "wefilter-error-container");
+        errorContainer.innerHTML = str;
+        document.getElementById("wefilter-container").appendChild(errorContainer);
+    }
+
+    clearError() {
+        let el = document.getElementById("wefilter-error-container");
+        if(el != null) {
+            el.parentNode.removeChild(el);
+        };
+    }
+
+    queryFeatures(group) {
+        this.clearResult();
+        let exploreUrl = "https://apis.wemap.asia/we-tools/explore?"
+        + "k=" + this._groups[group]["k"] + "&"
+        + "v=" + this._groups[group]["v"] + "&"
+        + "lat=" + this._userLocation.lat + "&"
+        + "lon=" + this._userLocation.lng + "&"
+        + "limit=20&d=1000";
+        makeRequest({
+            url: exploreUrl
+        }, (err, res) => {
+            
+        });
+    }
+
+    clearResult() {
+
+    }
+
 
 }
