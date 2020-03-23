c5542651b7833e0fb29d2b1fc550451de4caf88f
HieuTM <hieutm@fimo.edu.vn>
2020-03-23 16:56:59 +0700

    WeFilter

--- src/wemap/wefilter.js
+++ src/wemap/wefilter.js
@@ -1,58 +1,34 @@
 import { makeRequest } from '../util/ajax';
 export default class WeFilterControl {
     constructor(options) {
-        this._currentGroup = null;
-        this._userLocation = null;
+        this._layerIds = [
+            "poi-level-1",
+            "poi-level-2",
+            "poi-level-3"
+        ];
         this._groups = {
             "cuisine": {
                 "text": "Ẩm thực",
                 "fa-icon": "fa-cutlery",
-                "kv": [
-                    ["amenity", "restaurant"],
-                    ["amenity", "cafe"],
-                    ["amenity", "fast_food"],
-                    ["amenity", "food_court"],
-                    ["amenity", "fast_food"]
-
-                ]
+                "featureClasses": ["cafe", "restaurant", "fast_food", "food_court"]
             },
             "hotel": {
                 "text": "Nhà nghỉ",
                 "fa-icon": "fa-hotel",
-                "kv": [
-                    ["tourism", "hotel"],
-                    ["tourism", "guest_house"],
-                    ["tourism", "motel"]
-                ]
+                "featureClasses": ["hotel", "guest_house", "motel"]
             },
             "entertainment": {
                 "text": "Giải trí",
                 "fa-icon": "fa-glass",
-                "kv": [
-                    ["amenity", "nightclub"],
-                    ["amenity", "pub"],
-                    ["amenity", "theatre"],
-                    ["amenity", "casino"],
-                    ["amenity", "cinema"]
-                ]
+                "featureClasses": ["bar", "nightclub", "pub", "theatre", "casino", "cinema"]
             },
             "shopping": {
                 "text": "Mua sắm",
                 "fa-icon": "fa-shopping-bag",
-                "kv": [
-                    ["shop", "mall"],
-                    ["shop", "supermarket"],
-                    ["shop", "shoes"],
-                    ["shop", "jewelry"],
-                    ["shop", "fashion"],
-                    ["shop", "bakery"],
-                    ["shop", "convenience"],
-                    ["amenity", "marketplace"]
-                ]
+                "featureClasses": ["shop", "grocery", "alcohol_shop", "jewelry", "mall", "supermarket", "fashion", "convenience", "marketplace"]
             }
         };
-        this._locationMarker = null;
-        this._markers = [];
+        this._currentGroup = null;
     }
 
     onAdd(map) {
@@ -63,17 +39,7 @@ export default class WeFilterControl {
 
         let wefilterTitle = document.createElement("div");
         wefilterTitle.setAttribute("id", "wefilter-title");
-        wefilterTitle.innerHTML = "Khám phá xung quanh bạn";
-
-        let wefilterAqi = document.createElement("span");
-        wefilterAqi.setAttribute("id", "wefilter-aqi");
-        wefilterAqi.innerHTML = "AQI 310";
-        wefilterTitle.appendChild(wefilterAqi);
-
-        let wefilterTemp = document.createElement("span");
-        wefilterTemp.setAttribute("id", "wefilter-temp");
-        wefilterTemp.innerHTML = "<i class='fa fa-cloud'></i> 26°C";
-        wefilterTitle.appendChild(wefilterTemp);        
+        wefilterTitle.innerHTML = "Bộ lọc";
 
         wefilterContainer.appendChild(wefilterTitle);
 
@@ -91,11 +57,11 @@ export default class WeFilterControl {
             button.appendChild(icon);
             
             wefilterButtonContainer.appendChild(button);
-            wefilterButtonContainer.addEventListener("click", () => this.onGroupClick(group));
 
             let span = document.createElement("span");
             span.innerHTML = this._groups[group]["text"];
             wefilterButtonContainer.appendChild(span);
+            wefilterButtonContainer.addEventListener("click", () => this.onClickFilter(group))
 
             wefilterContainer.appendChild(wefilterButtonContainer);
         });
@@ -113,150 +79,36 @@ export default class WeFilterControl {
         return 'top-right';
     }
 
-    onGroupClick(group) {
-        if(this._userLocation == null) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                this._userLocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                this.explore(group);
-            }, () => {
-                this.printError("Không thể lấy vị trí của bạn");
-            });
-        } else {
-            this.explore(group);
-        }
-        
-    }
-
-    explore(group) {
-        // TODO: when removing this marker?
-        this._locationMarker = this.createMarker("wefilter-marker", "wefilter-marker-location", "fa fa-male")
-        .setLngLat([this._userLocation.lng, this._userLocation.lat])
-        .addTo(this._map);
-
-        if(this._currentGroup == null) { // user want to turn on
+    onClickFilter(group) {
+        if(this._currentGroup == null) {
             this._currentGroup = group;
             document.getElementById("wefilter-button-container-" + group).classList.add("wefilter-button-container-active");
-            this.queryFeatures(group);
         } else {
             document.getElementById("wefilter-button-container-" + this._currentGroup).classList.remove("wefilter-button-container-active");
-            if(this._currentGroup != group) { // user want to change
-                this.clearPrevResult();
-                this.queryFeatures(group);
+            if(this._currentGroup != group) {
                 this._currentGroup = group;
                 document.getElementById("wefilter-button-container-" + group).classList.add("wefilter-button-container-active");
-            } else { // user just click again to turn off 
+            } else {
                 this._currentGroup = null;
-                this.clearPrevResult();
-                this._locationMarker.remove();
-                this._locationMarker = null;
             }
         }
+        this.filter(this._currentGroup);
     }
 
-    printError(str) {
-        this.clearError();
-        let errorContainer = document.createElement("div");
-        errorContainer.setAttribute("id", "wefilter-error-container");
-        errorContainer.innerHTML = str;
-        document.getElementById("wefilter-container").appendChild(errorContainer);
-    }
-
-    clearError() {
-        let el = document.getElementById("wefilter-error-container");
-        if(el != null) {
-            el.parentNode.removeChild(el);
-        };
-    }
-
-    queryFeatures(group) {
-
-        let keyValuePairs = this._groups[group]["kv"];
-
-        let minLon = null;
-        let maxLon = null;
-        let minLat = null;
-        let maxLat = null;
-
-        let requestNum = 0;
-
-        keyValuePairs.forEach((pair) => {
-            let exploreUrl = this.createExploreUrl(this._userLocation.lat,  this._userLocation.lng, pair[0], pair[1], 1000, 20);
-            makeRequest({
-                url: exploreUrl,
-                method: "GET"
-            }, (err, res) => {
-                requestNum += 1;
-                if(res != undefined) {
-                    let points = JSON.parse(res);
-                    points.forEach(point => {
-                        let lat = parseFloat(point.lat);
-                        let lon = parseFloat(point.lon);
-                        (minLon == null || minLon > lon) && (minLon = lon);
-                        (minLat == null || minLat > lat) && (minLat = lat);
-                        (maxLon == null || maxLon < lon) && (maxLon = lon);
-                        (maxLat == null || maxLat < lat) && (maxLat = lat);
-                        
-                        let marker = this.createMarker("wefilter-marker", "wefilter-marker-" + group, "fa fa-map-marker").setLngLat([lon, lat]).addTo(this._map);
-                        marker.setPopup(this.createPopup(point, pair[1]));
-                        let markerEl = marker.getElement();
-                        markerEl.addEventListener('mouseenter', () => marker.togglePopup());
-                        markerEl.addEventListener('mouseleave', () => marker.togglePopup());
-                        this._markers.push(marker);
-                    });
-                    if(requestNum == keyValuePairs.length) {
-                        this._map.fitBounds([
-                            [minLon, minLat],
-                            [maxLon, maxLat]
-                        ]);
-                    }               
-                }
-                
-            });
-
+    filter(group) {
+        let rules = [];
+        if(group != null) {
+            rules = [
+                "in",
+                "class"
+            ];
+            rules = rules.concat(this._groups[group]["featureClasses"]);
+        } else {
+            rules = ["all"];
+        }
+        this._layerIds.forEach(layerId => {
+            this._map.setFilter(layerId, rules);
         });
-        
-    }
-
-    createMarker(elClass, elId, iconClass) {
-        let markerEl = document.createElement("div");
-        markerEl.setAttribute("class", elClass);
-        markerEl.setAttribute("id", elId);
-        let markerIcon = document.createElement("i");
-        markerIcon.setAttribute("class", iconClass);
-        markerEl.appendChild(markerIcon);
-        let marker = new wemapgl.Marker(markerEl);
-        return marker;
-    }
-
-    createPopup(point, value) {
-        console.log(point);
-        console.log(value);
-        console.log(point.address[value]);
-        let popup = new wemapgl.Popup({offset: 15}).setHTML(
-            '<p>' + point.address[value] + '</p>'
-        );
-        return popup;
-    }
-
-    clearPrevResult() {
-        this._markers.forEach(marker => marker.remove());
-        this._markers = [];
-    }
-
-    createExploreUrl(lat, lon, k, v, distance, limit) {
-        // TODO: move to config file
-        let url = "https://apis.wemap.asia/we-tools/explore?"
-        + "k=" + k + "&"
-        + "v=" + v + "&"
-        + "lat=" + lat + "&"
-        + "lon=" + lon + "&"
-        + "limit=" + limit + "&"
-        + "d=" + distance;
-        return url;
     }
 
 }
